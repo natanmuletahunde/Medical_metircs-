98320b265f24e0fa97d0b1a33ef491924a9335e0
pivnati <natanmuleta911@gmail.com>
2024-09-19 22:14:24 -0700

    create the components folder and inside it update the footer file

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer className="bg-indigo-600 text-white text-center p-4">
+        &copy; 2024 Medical Metric - All Rights Reserved
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
