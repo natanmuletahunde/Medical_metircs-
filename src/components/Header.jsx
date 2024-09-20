5b23f2eed0769b5f63f5fbebc3feb68ca80a2f30
pivnati <natanmuleta911@gmail.com>
2024-09-19 22:13:33 -0700

    m update the header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+      <header className="bg-blue-500 text-white p-4 text-center text-3xl font-bold">
+        Medical Metric Calculator
+      </header>
+    );
+  };
+  
+  export default Header;
+  
